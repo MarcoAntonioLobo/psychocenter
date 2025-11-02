56b125cf7a24c8a0ab9b99a3964234796042eef2
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-11-02 12:00:59 +0000

    Project created

---
+++ src/test/java/com/psy/psychocenter/PsychocenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psy.psychocenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychocenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
